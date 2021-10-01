47e23f0c1672d2fc1d5e93a21cc63e31df873e38
Daniel Barnes <dabarnes2b@gmail.com>
2021-10-01 12:38:47 -0700

    add link

--- go/delSnaps/main.go
+++ go/delSnaps/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	// https://pkg.go.dev/github.com/aws/aws-sdk-go-v2
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
